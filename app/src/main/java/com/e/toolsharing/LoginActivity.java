b1d08d9f4466a45564f76616d3b8ccaa32ce22e6
Dilip Reddy <merugudilipreddy@gmail.com>
2020-09-20 21:58:58 -0400

    login and register activities

--- app/src/main/java/com/e/toolsharing/LoginActivity.java
+++ app/src/main/java/com/e/toolsharing/LoginActivity.java
@@ -2,13 +2,26 @@ package com.e.toolsharing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        register = findViewById(R.id.textView2);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/e/toolsharing/MainActivity.java
+++ app/src/main/java/com/e/toolsharing/MainActivity.java
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-              //  Intent i = new Intent(MainActivity.this,LoginActivity.class);
-              //  startActivity(i);
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
 
 
             }
