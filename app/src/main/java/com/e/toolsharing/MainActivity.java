0bfa696149b990baa78257ce23d1999d12ab4848
sushmithavootla <sushmithavootla2@gmail.com>
2020-09-29 15:26:40 -0400

    Added animation to splash screen

--- app/src/main/java/com/e/toolsharing/MainActivity.java
+++ app/src/main/java/com/e/toolsharing/MainActivity.java
@@ -6,26 +6,37 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT=3000;
+    Animation top_anim,btm_anim;
+    ImageView imageView;
+    TextView textlogo,texttagline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setVisibility(GONE);
+        imageView = findViewById(R.id.imageView);
+        textlogo = findViewById(R.id.textView4);
+        texttagline = findViewById(R.id.textView5);
+        top_anim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        btm_anim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageView.setAnimation(top_anim);
+        textlogo.setAnimation(btm_anim);
+        texttagline.setAnimation(btm_anim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i = new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(i);
-
-
             }
-        },SPLASH_TIME_OUT);
+        },3000);
     }
 }
