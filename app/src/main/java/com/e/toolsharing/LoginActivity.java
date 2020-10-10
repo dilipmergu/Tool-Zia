be48bf2cb41f0e80a410f425166a82378e60695e
V744 <vivekmalhotra676@gmail.com>
2020-10-09 21:14:56 -0400

    added Registration files

--- app/src/main/java/com/e/toolsharing/LoginActivity.java
+++ app/src/main/java/com/e/toolsharing/LoginActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.e.toolsharing.activities.RegistrationActivity;
+
 public class LoginActivity extends AppCompatActivity {
 
 
@@ -19,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,Register.class);
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivity(intent);
             }
         });
