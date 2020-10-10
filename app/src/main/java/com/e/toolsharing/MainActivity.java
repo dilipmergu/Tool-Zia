360255d521c2ae830051f0dbd42d757d75606c16
sushmithavootla <sushmithavootla2@gmail.com>
2020-10-09 21:37:57 -0400

    updated splash screen

--- app/src/main/java/com/e/toolsharing/MainActivity.java
+++ app/src/main/java/com/e/toolsharing/MainActivity.java
@@ -14,29 +14,11 @@ import android.widget.TextView;
 import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
-    Animation top_anim,btm_anim;
-    ImageView imageView;
-    TextView textlogo,texttagline;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
-        textlogo = findViewById(R.id.textView4);
-        texttagline = findViewById(R.id.textView5);
-        top_anim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        btm_anim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
-        imageView.setAnimation(top_anim);
-        textlogo.setAnimation(btm_anim);
-        texttagline.setAnimation(btm_anim);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(i);
-            }
-        },3000);
     }
 }
