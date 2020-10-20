fbaf77a27456351a181a51be236751a804f16ff8
priyatham <saipriyathamvarma@gmail.com>
2020-10-20 10:52:36 -0400

    made modifications to ScarchFragment

--- app/src/main/java/com/e/toolsharing/ScarchFragment.java
+++ app/src/main/java/com/e/toolsharing/ScarchFragment.java
@@ -1,11 +1,108 @@
 package com.e.toolsharing;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.e.toolsharing.adapters.SearchAdapter;
+import com.e.toolsharing.models.HomeDataPojo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class ScarchFragment extends Fragment {
+    GridView gridview;
+    List<HomeDataPojo> a1;
+    ProgressDialog progressDialog;
+    SearchAdapter searchAdapter;
+    EditText et_search;
+    View view;
+
+
     public static ScarchFragment scarchFragment() {
         ScarchFragment fragment = new ScarchFragment();
         return fragment;
     }
-}
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_search, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Search");
+        progressDialog=new ProgressDialog(getContext());
+
+
+        gridview=(GridView)view.findViewById(R.id.gridview);
+        a1= new ArrayList<>();
+        Query query = FirebaseDatabase.getInstance().getReference("Products");
+        query.addListenerForSingleValueEvent(valueEventListener);
+
+        et_search = (EditText) view.findViewById(R.id.et_search);
+        et_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+                String text = et_search.getText().toString().toLowerCase(Locale.getDefault());
+                searchAdapter.filter(text);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+
+
+
+        return view;
+    }
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            progressDialog.dismiss();
+            a1.clear();
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    HomeDataPojo homeDataPojo = snapshot.getValue(HomeDataPojo.class);
+                    a1.add(homeDataPojo);
+                }
+                searchAdapter = new SearchAdapter(a1, getActivity());
+                gridview.setAdapter(searchAdapter);
+
+            }
+            else {
+                Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            progressDialog.dismiss();
+
+        }
+    };
+}
