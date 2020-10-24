5e42fa608fff559b16ade0e3d3d2dbcea161733b
spandana <spandysama3@gmail.com>
2020-10-23 21:14:38 -0400

    Added HistoryFragment.java

--- app/src/main/java/com/e/toolsharing/HistoryFragment.java
+++ app/src/main/java/com/e/toolsharing/HistoryFragment.java
@@ -1,10 +1,99 @@
 package com.e.toolsharing;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.e.toolsharing.activities.MyProfileActivity;
+import com.e.toolsharing.activities.Utils;
+import com.e.toolsharing.adapters.HistoryAdapter;
+import com.e.toolsharing.models.HomeDataPojo;
+import com.e.toolsharing.models.HomePojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HistoryFragment extends Fragment {
+    GridView gridview;
+    List<HomeDataPojo> a1;
+    SharedPreferences sharedPreferences;
+    String session;
+    HistoryAdapter historyAdapter;
+    ProgressDialog progressDialog;
+    View view;
+
     public static HistoryFragment historyFragment() {
         HistoryFragment fragment = new HistoryFragment();
         return fragment;
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_history, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("History");
+
+        sharedPreferences =getActivity().getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("user_name", "def-val");
+
+        a1= new ArrayList<>();
+
+
+        gridview=(GridView)view.findViewById(R.id.gridview);
+        progressDialog=new ProgressDialog(getActivity());
+        progressDialog.setTitle("Please Wait data is being Loaded");
+        progressDialog.show();
+        Query query = FirebaseDatabase.getInstance().getReference("Products")
+                .orderByChild("booked_by")
+                .equalTo(session);
+        query.addListenerForSingleValueEvent(valueEventListener);
+
+
+        return view;
+    }
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            progressDialog.dismiss();
+            a1.clear();
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    HomeDataPojo homeDataPojo = snapshot.getValue(HomeDataPojo.class);
+                    a1.add(homeDataPojo);
+                }
+                //Toast.makeText(getContext(), ""+a1.size(), Toast.LENGTH_SHORT).show();
+                historyAdapter = new HistoryAdapter(a1,getActivity());
+                gridview.setAdapter(historyAdapter);
+
+            }
+            else {
+                Toast.makeText(getContext(), "No data Found", Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            progressDialog.dismiss();
+
+        }
+    };
+
 }
